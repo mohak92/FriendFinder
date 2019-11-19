f3afe89e54810a90b0399634d2e276116cc0d743
mohak92 <tamhane.mohak@gmail.com>
2019-11-19 09:07:56 -0500

    server js implementated

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+var express = require("express");
+var path = require("path");
 
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+var friends = require("./app/data/friends.js");
+
+//require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function () {
+    console.log("Sever is listening at http://localhost:" + PORT);
+})
