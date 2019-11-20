140997ac6de4cd8b9e946b6f5684ce2ba00dcd4b
mohak92 <tamhane.mohak@gmail.com>
2019-11-20 08:13:09 -0500

    optimizations server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -7,9 +6,7 @@ var PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-var friends = require("./app/data/friends.js");
-
-//require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function () {
