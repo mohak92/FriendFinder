cd1949f0896776c4cd383981129da69f943ef333
mohak92 <tamhane.mohak@gmail.com>
2019-11-20 09:24:21 -0500

    data and routing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,16 @@
 var path = require("path");
+var friends = require("../data/friends");
 
 module.exports = function (app) {
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
-    })
+    });
 
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    app.get("/api/friends", function(req, res) {
+        return res.json(friends.friends);
     })
 }
